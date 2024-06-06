8935730fc8eeab5b3d64d71580f1827b8b01e580
yurabula <bulayuram@gmail.com>
2024-06-06 15:44:05 +0300

    Some structure changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
-import MyAccount from './components/MyAccount/MyAccount';
-import ObjectDog from './components/ObjectDog/ObjectDog';
-import AddDog from './components/AddDog/AddDog';
-import LogIn from './components/LogIn/LogIn';
-import CreateAccount from './components/CreateAccount/CreateAccount';
-import FooterReg from './components/Footer/Footer';
-import ObjectDogElem from './components/ObjectDog/ObjectDogElem';
-import Footer from './components/kjn/Footer';
-import './normalize.scss';
-import './App.scss'
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import MyAccount from "./components/MyAccount/MyAccount";
+import ObjectDog from "./components/ObjectDog/ObjectDog";
+import AddDog from "./components/AddDog/AddDog";
+import Footer from "./components/kjn/Footer";
+import "./normalize.scss";
+import "./App.scss";
 
 const App = () => {
-
-    return (
-        <div className='App'>
-            {/* <Header/>
-            <CreateAccount/>
-            <MyAcc/>
-            <AddDog/>
-            <FooterReg/> */}
-
-        <Router>
-            {/* <Routes>
-            <Route path="/" element={<MyAcc />} />
-            <Route path="/create-account" element={<CreateAccount />} />
-        </Routes>
-        <FooterReg /> */}
-            <div>
-                <Header />
-                <MyAccount />
-                <ObjectDog />
-                <AddDog></AddDog>
-
-            </div>
-            <Footer />
-        </Router>
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <Header />
+          <MyAccount />
+          <ObjectDog />
+          <AddDog></AddDog>
         </div>
-    );
-}
+        <Footer />
+      </Router>
+    </div>
+  );
+};
 
 export default App;
