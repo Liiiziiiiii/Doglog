ac5e8f3ebf374883001477560d042cb83bdf2e62
yurabula <bulayuram@gmail.com>
2024-06-12 18:07:15 +0300

    Tree desogn changes

--- frontend/src/components/Tree/Tree.jsx
+++ frontend/src/components/Tree/Tree.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import Dog from './DogModel';
+import React, { useState } from "react";
+import Dog from "./DogModel";
+import "./Tree.css";
 
 const Tree = ({ dog }) => {
   const [currentDog, setCurrentDog] = useState(dog);
@@ -13,29 +14,40 @@ const Tree = ({ dog }) => {
 
   const renderDog = (dog, depth = 0) => {
     if (!dog) return null;
-    
+
     return (
-      <div style={{ marginLeft: `${depth * 20}px` }}>
-        <div onClick={() => addParent('father', prompt('Father\'s name'))}>
-          Father: {dog.parents.father ? dog.parents.father.name : "Add Father"}
+      <div className="Tree">
+        <div className="TreeObject">Name: {dog.name}</div>
+        <div className="ObjectPair">
+          <div>
+          Father:{" "}
+            {dog.parents.father ? dog.parents.father.name : "Add Father"}
+          <div
+            className="TreeObject"
+            onClick={() => addParent("father", prompt("Father's name"))}
+          >
+          </div>
+          </div>
+          
+          <div>
+          Mother:{" "}
+              {dog.parents.mother ? dog.parents.mother.name : "Add Mother"}
+            <div
+              className="TreeObject"
+              onClick={() => addParent("mother", prompt("Mother's name"))}
+            >
+              
+            </div>
+          </div>
         </div>
-        <div onClick={() => addParent('mother', prompt('Mother\'s name'))}>
-          Mother: {dog.parents.mother ? dog.parents.mother.name : "Add Mother"}
-        </div>
-        <div>
-          Name: {dog.name}
-        </div>
+
         {renderDog(dog.parents.father, depth + 1)}
         {renderDog(dog.parents.mother, depth + 1)}
       </div>
     );
   };
 
-  return (
-    <div>
-      {renderDog(currentDog)}
-    </div>
-  );
+  return <div>{renderDog(currentDog)}</div>;
 };
 
 export default Tree;
