ec1ce5a380b9cf36f9d2bb6fbfd26315328a33f5
yurabula <bulayuram@gmail.com>
2024-07-05 15:06:59 +0300

    add dog page

--- frontend/src/components/AddDog/AddDog.jsx
+++ frontend/src/components/AddDog/AddDog.jsx
@@ -3,18 +3,26 @@ import './AddDog.scss';
 import add_photo_icon from "../../images/add_photo_icon.png"
 import Header from '../Header/Header';
 import FooterReg from '../Footer/Footer';
+import axios from 'axios';
 
 
 const AddDog = ({ closeModal, onAddDog, dog }) => {
     const [formData, setFormData] = useState({
-        name_dog: '',
-        breed_dog: '',
-        coat_dog: '',
-        dob_dog: '',
-        gender_dog: 'male',
-        height_dog: 0,
-        weight_dog: 0,
-        number_dog: 0,
+        name: '',
+        breed: '',
+        wool: '',
+        age:'',
+        dateBirth: '',
+        sex: '',
+        growth: 0,
+        weight: 0,
+        ksy: 0,
+        namenursery: "",
+        chip: "",
+        photo: "",
+        userId: 0,
+        mother_Id: 0,
+        father_Id: 0
     });
 
     useEffect(() => {
@@ -31,10 +39,18 @@ const AddDog = ({ closeModal, onAddDog, dog }) => {
         }));
     };
 
-    const handleSubmit = (e) => {
+    async function handleSubmit (e) {
         e.preventDefault();
-        onAddDog(formData);
-        closeModal();
+        console.log(formData)
+        try {
+            const response = await axios.post('http://localhost:5254/api/Dog', formData);
+            console.log('Response:', response);
+            if(response.status === 201){
+                console.log("Added sucsesfully")
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
     };
 
 
@@ -52,42 +68,43 @@ const AddDog = ({ closeModal, onAddDog, dog }) => {
                     </div>
                     <div className='form_fields'>
                         <div>
-                            <input className='basicInput' placeholder="Кличка:" type="text" id="name_dog" name="name_dog" value={formData.name_dog} onChange={handleChange} />
+                            <input className='basicInput' placeholder="Кличка:" type="text" id="name_dog" name="name" value={formData.name} onChange={handleChange} />
                         </div>
 
                         <div>
-                            <input className='basicInput' placeholder="Порода:" type="text" id="breed_dog" name="breed_dog" value={formData.breed_dog} onChange={handleChange} />
+                            <input className='basicInput' placeholder="Порода:" type="text" id="breed_dog" name="breed" value={formData.breed} onChange={handleChange} />
                         </div>
 
                         <div>
-                            <input className='basicInput' placeholder="Шерсть:" type="text" id="coat_dog" name="coat_dog" value={formData.coat_dog} onChange={handleChange} />
+                            <input className='basicInput' placeholder="Шерсть:" type="text" id="coat_dog" name="wool" value={formData.wool} onChange={handleChange} />
                         </div>
 
                         <div>
-                            <input className='basicInput' placeholder="Дата народження:"type="date" id="dob_dog" name="dob_dog" value={formData.dob_dog} onChange={handleChange} />
+                            <input className='basicInput' placeholder="Дата народження:"type="date" id="dob_dog" name="dateBirth" value={formData.dateBirth} onChange={handleChange} />
                         </div>
 
                         <div className='wrapper'>
                             <label className='gender_dog'>Стать:</label>
-                            <select id="gender_and_dog" name="gender_dog">
-                                <option value="male">Чоловіча</option>
-                                <option value="female">Жіноча</option>
-                            </select>
+                            <select id="sex" name="sex" value={formData.sex} onChange={handleChange}>
+                                    <option value="">Виберіть стать</option>
+                                    <option value="male">Чоловіча</option>
+                                    <option value="female">Жіноча</option>
+                                </select>
                         </div>
 
                         <div className='wrapper'>
                             <label className='height_dog'>Зріст:</label>
-                            <input type="number" id="height_dog" name="height_dog" placeholder="Введіть ріст в см" value={formData.height_dog} onChange={handleChange} min="0" />
+                            <input type="number" id="height_dog" name="growth" placeholder="Введіть ріст в см" value={formData.growth} onChange={handleChange} min="0" />
                         </div>
 
                         <div className='wrapper'>
                             <label className="weight_dog">Вага:</label>
-                            <input  type="number" id="weight_dog" name="weight_dog" placeholder="Введіть вагу в кг" value={formData.weight_dog} onChange={handleChange} min="0" ></input>
+                            <input  type="number" id="weight_dog" name="weight" placeholder="Введіть вагу в кг" value={formData.weight} onChange={handleChange} min="0" ></input>
                         </div>
 
                         <div className='wrapper' >
                             <label className="number_dog">Чіп:</label>
-                            <input type="number" id="number_dog" name="number_dog" placeholder="Введіть номер чипу (необов'язково)" value={formData.number_dog} onChange={handleChange} min="0" />
+                            <input type="number" id="number_dog" name="chip" placeholder="Введіть номер чипу (необов'язково)" value={formData.chip} onChange={handleChange} min="0" />
                         </div>
                         <button className='submit_button'>Save</button>
                     </div>
