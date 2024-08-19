f6a1200f0f65663e1c48ff0678c8d4bbda5cba04
yurabula <bulayuram@gmail.com>
2024-08-20 01:44:08 +0300

    Add registration check

--- frontend/src/components/LogIn/LogIn.jsx
+++ frontend/src/components/LogIn/LogIn.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
@@ -9,6 +9,7 @@ import { Footer } from '../Footer';
 
 const LogIn = () => {
     const navigate = useNavigate();
+    const [userRegistered, setUserRegistered] = useState(true);
 
     const formik = useFormik({
         initialValues: {
@@ -28,13 +29,14 @@ const LogIn = () => {
                     console.log("Login successful", response.data);
 
                     localStorage.setItem("loggedIn", true);
-                    localStorage.setItem("token", response.data); 
-                    localStorage.setItem("UserId", response.data); 
+                    localStorage.setItem("token", response.token); 
+                    localStorage.setItem("UserId", response.userId); 
 
-                    navigate(`/myaccount/${response.data}`);
+                    navigate(`/myaccount/${response.data.userId}`);
                 }
             } catch (error) {
                 console.error('Error:', error);
+                setUserRegistered(false);
             }
         },
     });
@@ -54,20 +56,47 @@ const LogIn = () => {
         );
     };
 
+    const UserNotRegisteredAttention = ({ userRegisteredState }) => {
+        if (!userRegisteredState) {
+            return <p className="error-message">Користувач не зареєстрований</p>;
+        } else {
+            return null;
+        }
+    };
+
     return (
         <>
             <form className="myacc" onSubmit={formik.handleSubmit}>
                 <div>
                     <h1>Мій Акаунт</h1>
                     <div>
-                        <input type="text" id="Name" name="Name" placeholder="Name" className="rounded-input" value={formik.values.Name} onChange={formik.handleChange} required />
+                        <input 
+                            type="text" 
+                            id="Name" 
+                            name="Name" 
+                            placeholder="Name" 
+                            className="rounded-input" 
+                            value={formik.values.Name} 
+                            onChange={formik.handleChange} 
+                            required 
+                        />
                     </div>
                     <div>
-                        <input type="password" id="Password" name="Password" placeholder="Password" className="rounded-input" value={formik.values.Password} onChange={formik.handleChange} required  />
+                        <input 
+                            type="password" 
+                            id="Password" 
+                            name="Password" 
+                            placeholder="Password" 
+                            className="rounded-input" 
+                            value={formik.values.Password} 
+                            onChange={formik.handleChange} 
+                            required  
+                        />
                     </div>
                     <div className="forgot-password">
                         <p><a href="#">Забули пароль?</a></p>
                     </div>
+                    <UserNotRegisteredAttention userRegisteredState={userRegistered} />
                     <div>
                         <button type="submit" className="login-button">Увійти</button>
                     </div>
