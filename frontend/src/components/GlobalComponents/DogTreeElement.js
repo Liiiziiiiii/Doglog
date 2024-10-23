954ca95a6ec2f52ec34facb689e8c416b4d5dfa1
yurabula <bulayuram@gmail.com>
2024-10-23 20:31:00 +0300

    Add aditional style

--- frontend/src/components/GlobalComponents/DogTreeElement.js
+++ frontend/src/components/GlobalComponents/DogTreeElement.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState} from 'react';
 import AddPicture from "../../images/add_picture.png";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import PlusIcon from "../../images/plus_icon.png"
+import TextField from '@mui/material/TextField';
+
 import "./DogTreeElement"
+import "./DogTreeElementAddition.css"
 
 const DogTreeElement = ({ name, dog }) => {
   if (!dog) return null;
+  const [isAditionalInfoOpen, setIsAditionalInfoOpen] = useState(false)
+  const [selectedDate, setSelectedDate] = useState(0)
 
-  //console.log("Dog photo URL:", dog.photo);
+  const handleAdditionalInfoClick=()=>{
+    setIsAditionalInfoOpen(!isAditionalInfoOpen)
+  }
 
   return (
     <div className="DogTreeElement">
@@ -16,6 +26,50 @@ const DogTreeElement = ({ name, dog }) => {
         className="AddPicture" 
       />
       <p>{dog.name}</p>
+
+      <div onClick={handleAdditionalInfoClick} className='isAditionalInfoButton'>
+          <img className='isAditionalInfoButtonImage' src={PlusIcon}/>
+      </div>
+
+      {isAditionalInfoOpen 
+      ? <div className='aditionalInfoWrapper'>
+          <DatePicker
+              selected={selectedDate}
+              onChange={(date) => setSelectedDate(date)}
+              dateFormat="dd/MM/yyyy"
+              placeholderText="Оберіть дату"
+              isClearable
+              className='aditionalInfoDataPicker'
+          />
+          <div className='aditionalInfoInputWrapper'>
+              <p>Екстер'єр:</p>
+              <input 
+                  className='aditionalInfoInput' 
+                  type="text" 
+                  placeholder="Екстер'єр"
+              />
+          </div>
+          <div className='aditionalInfoInputWrapper'>
+              <p>Дипломи:</p>
+              <input 
+                  className='aditionalInfoInput' 
+                  type="file"
+              />
+          </div>
+          <div className='aditionalInfoInputWrapper'>
+              <p>Власник:</p>
+              <input 
+                  className='aditionalInfoInput' 
+                  type="text" 
+                  placeholder="Власник"
+              />
+          </div>
+          
+      </div> : 
+      <>
+      </>
+}
+
     </div>
   );
 };
